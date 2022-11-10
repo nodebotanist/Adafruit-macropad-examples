e27bdb35f54bf78611a821dc6b320a3542a09ff9
Kassian Wren <the@nodebotani.st>
2022-11-10 12:56:14 -0600

    Layers work

--- layers/buttonConfigs.py
+++ layers/buttonConfigs.py
@@ -1,11 +1,161 @@
 BUTTON_CONFIGS = [
     [
         (
-            [255, 255, 0],
+            (255, 0, 255),
             [
                 "macropad.keyboard_layout.write('git status')",
                 "macropad.keyboard.send(macropad.Keycode.ENTER)"
             ]
-        )
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 255),
+            [
+                "macropad.keyboard_layout.write('git status')",
+                "macropad.keyboard.send(macropad.Keycode.ENTER)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(1)"
+            ]
+        ),
+    ],
+    [
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
+        (
+            (255, 0, 0),
+            [
+                "changeLayer(0)"
+            ]
+        ),
     ]
 ]
--- layers/code.py
+++ layers/code.py
@@ -7,13 +7,25 @@ import buttonConfigs
 # get rid of the argument if you're not using dvorak
 macropad = MacroPad(layout_class=KeyboardLayout)
 text_lines = macropad.display_text(title="Hello, World!")
-current_layer = 0
+global current_layer
+current_layer=0
+
+def changeLayer(layer_number):
+    global current_layer
+    print(current_layer)
+    if(layer_number < len(buttonConfigs.BUTTON_CONFIGS)):
+        current_layer = layer_number
+    else:
+        current_layer = 0
 
 while (True):
+    global current_layer
+    for index in range(0,12):
+        macropad.pixels[index] = buttonConfigs.BUTTON_CONFIGS[current_layer][index][0]
     key_event = macropad.keys.events.get()
     if key_event and key_event.pressed:
-        color = buttonConfigs.BUTTON_CONFIGS[current_layer][key_event.key_number][0]
         commands = buttonConfigs.BUTTON_CONFIGS[current_layer][key_event.key_number][1]
         for command in commands:
             eval(command)
     time.sleep(0.2)
+
